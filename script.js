7c895f96986b6a1408a4945b8c6c2864c97820da
unknown <gunaorusty@gmail.com>
2023-08-08 15:38:13 +0800

    add select color

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-// display range input in settings
-
 const rangeInput = document.getElementById('range');
 
-rangeInput.addEventListener('input', () => document.getElementById('size').textContent = `${rangeInput.value} x ${rangeInput.value}`)
+// display range input in settings
+rangeInput.addEventListener('input', () => {
+    document.getElementById('size').textContent = `${rangeInput.value} x ${rangeInput.value}`
+});
+
+// color picker
+const colorPickerIcons = document.querySelectorAll(".material-symbols-outlined");
+const colorPickers = document.querySelectorAll("input[type='color']");
+const bgColorPicker = document.querySelector(".bg-color-picker");
+const board = document.getElementById('board');
+
+colorPickerIcons.forEach((icon, index) => {
+    icon.addEventListener("click", () => {
+        colorPickers[index].click();
+        colorPickers[index].style.border = 'transparent !important';
+    });
+});
+
+colorPickers.forEach((colorPicker, index) => {
+    colorPicker.addEventListener("input", () => {
+        const selectedColor = colorPicker.value;
+        colorPickerIcons[index].style.color = selectedColor;
+        colorPickerIcons[index].style.backgroundColor = `rgba(89, 152, 219, 0.2)`;
+        colorPickerIcons[index].style.border = `solid 1px rgba(0, 0, 0, 0.1)`;
+
+        bgColorPicker.addEventListener('input', () => {
+            board.style.backgroundColor = selectedColor;
+        })
+    });
+});
+
