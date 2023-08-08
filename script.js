fccb8b0e7192400e95df68d0a93a1c15220724cb
unknown <gunaorusty@gmail.com>
2023-08-08 19:17:49 +0800

    add grid to board

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const rangeInput = document.getElementById('range');
-
-// display range input in settings
-rangeInput.addEventListener('input', () => {
-    document.getElementById('size').textContent = `${rangeInput.value} x ${rangeInput.value}`
-});
-
-// color picker
 const colorPickerIcons = document.querySelectorAll(".material-symbols-outlined");
 const colorPickers = document.querySelectorAll("input[type='color']");
 const bgColorPicker = document.querySelector(".bg-color-picker");
 const board = document.getElementById('board');
+const boardBody = document.querySelector('.board-body');
 
+// display range input in settings
+
+document.addEventListener('DOMContentLoaded', () => {
+    rangeInput.addEventListener('input', () => {
+        document.getElementById('size').textContent = `${rangeInput.value} x ${rangeInput.value}`;
+
+        // setup grid in board
+        boardBody.style.gridTemplateRows = `repeat(${rangeInput.value}, 1fr`;
+        boardBody.style.gridTemplateColumns = `repeat(${rangeInput.value}, 1fr`;
+
+        boardBody.innerHTML = '';
+
+        for (let i = 0; i < Number(rangeInput.value) * Number(rangeInput.value); i++) {
+            const gridItems = document.createElement('div');
+            boardBody.appendChild(gridItems);
+            gridItems.style.backgroundColor = 'black';
+            gridItems.style.border = '1px white solid';
+        }
+    });
+});
+
+
+
+
+// color picker when clicked
 colorPickerIcons.forEach((icon, index) => {
     icon.addEventListener("click", () => {
         colorPickers[index].click();
@@ -18,6 +37,7 @@ colorPickerIcons.forEach((icon, index) => {
     });
 });
 
+// color picker color display
 colorPickers.forEach((colorPicker, index) => {
     colorPicker.addEventListener("input", () => {
         const selectedColor = colorPicker.value;
@@ -30,4 +50,3 @@ colorPickers.forEach((colorPicker, index) => {
         })
     });
 });
-
